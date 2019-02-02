940544bb405967484be637e192815e51c4622c86
byonghun <byonghunkwak@gmail.com>
2019-02-02 14:29:07 -0800

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ReactDOM from 'react-dom';
-import TestComponent from 'gpservices/components/index.js';
+import {Header} from './Components/Header';
+import {Body} from './Components/Body';
+import {Footer} from './Components/Footer';
 
 class App extends Component {
+  state={
+    innerWidth: window.innerWidth,
+    innerHeight: window.innerHeight
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.windowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.windowResize)
+  }
+
+  windowResize = ({currentTarget: {innerWidth, innerHeight}}) => {
+    if(innerWidth !== this.state.innerWidth ||
+      innerHeight !== this.state.innerHeight) {
+      this.setState({innerWidth, innerHeight})
+    }
+  }
+
   render() {
     return (
-      <TestComponent/>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <Header
+          width={this.state.innerWidth}
+        />
+        <Body
+          width={this.state.innerWidth * .90}
+          height={this.state.innerHeight - 170} // this height will be removed as we add onto the body
+        />
+        <Footer
+          width={this.props.width}
+        />
+      </div>
     );
   }
 }
